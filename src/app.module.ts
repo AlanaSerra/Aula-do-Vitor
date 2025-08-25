e78c63210106f1b4ac73a80a9de3fa7e7a25a1d4
AlanaSerra <alana.scherer@estudante.ifms.edu.br>
2025-08-25 10:55:38 -0400

    aua 5/08

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ScenarioService } from './modules/scenario/scenario.service';
+import { ScenarioModule } from './modules/scenario/scenario.module';
 
 @Module({
-  imports: [],
+  imports: [ScenarioModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, ScenarioService],
 })
 export class AppModule {}
