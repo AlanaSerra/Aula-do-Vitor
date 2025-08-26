9c88514b9dccb03f663d3ae16e515e48fe3a2b68
AlanaSerra <alana.scherer@estudante.ifms.edu.br>
2025-08-26 12:36:04 -0400

    aula 26

--- src/modules/scenario/repository/create-scenario.repository.ts
+++ src/modules/scenario/repository/create-scenario.repository.ts
@@ -3,7 +3,7 @@ import {PrismaService} from "src/shared/databases/prisma.database";
 import { CreateScenarioDto } from "../dto/create-scenario.dto";
 
 @Injectable()
-export class CreatScenarioRepository {
+export class CreateScenarioRepository {
     constructor (private readonly prisma: PrismaService) {}
     async create (data: CreateScenarioDto) {
         const scenario = await this.prisma.scenario.create ({
