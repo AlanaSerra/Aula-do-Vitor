fa633ad0aea3ae3a33e2d7231367233f72856ded
AlanaSerra <alana.scherer@estudante.ifms.edu.br>
2025-09-01 11:05:39 -0400

    aula 01/09

---
+++ src/modules/scenario/repository/list.ts
@@ -0,0 +1,13 @@
+import { PrismaService } from './../../../shared/databases/prisma.database';
+import { Injectable } from '@nestjs/common';
+
+
+@Injectable()
+export class ListRepository {
+  constructor( private readonly prisma: PrismaService) {}
+
+  async list () {
+    const scenario = await this.prisma.scenario.findMany();
+    return scenario
+  }
+}
